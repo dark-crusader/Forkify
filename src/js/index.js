21375a71eb646d3025338cbc07e6e7121d71c656
dark-crusader <shubham.rubal31@gmail.com>
2020-05-27 05:55:50 +0530

    Implemented search Views

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,39 @@
 // Global app controller
 
 import Search from './models/Search';
+import {elements} from './views/base';
+import * as searchView from './views/searchView';
 
-const search = new Search('pasta');
-console.log(search);
-search.getResults();
+/** Stores current state of the app
+ * Search object
+ * Current recipe
+ * Shopping list
+ * Favourite recipies
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // TODO: Getting query from searchView
+    const query = searchView.getInput();
+    // If exists
+    if (query) {
+        // Modify state
+        state.search = new Search(query);
+        // TODO: Change UI
+        searchView.clearResults();
+        searchView.clearInput();
+
+        // Search for recipes
+        await state.search.getResults();
+
+        // TODO: Render results to UI
+        searchView.renderResults(state.search.result);
+        
+    }
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
